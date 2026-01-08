d74a85b882d6e5597893becf55406dc13178602f
Blipscan <thamesentertainment@gmail.com>
2026-01-08 15:40:19 -0500

    Serve client app from /

--- server/server.js
+++ server/server.js
@@ -11,7 +11,12 @@ const { buildCookbook } = require('./cookbook');
 
 const app = express();
 app.use(express.json({ limit: '10mb' }));
-app.use(express.static(__dirname));
+const CLIENT_DIR = path.join(__dirname, "..", "client");
+app.use(express.static(CLIENT_DIR));
+ 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(CLIENT_DIR, "index.html"));
+});
 
 // ============================================================
 // CONFIGURATION
